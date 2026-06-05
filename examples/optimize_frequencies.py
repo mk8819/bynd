30cc2d8aa6f8f709d226e3652bb4eb0fa40ead8e
Matthias <mkick@mit.edu>
2026-06-05 13:39:01 +0200

    improve readability of example scripts

--- examples/optimize_frequencies.py
+++ examples/optimize_frequencies.py
@@ -1,180 +1,150 @@
-#############################################################################################################
-# 
-# This is essentially a wrapper around different functions which perform the entire job
-# The complexity is needed cause of the RT-TDDFT data format of FHIaims.
-#
-# This is a wrapper for FHIaims data only, this will not work for other RT-TDDFT signals from other codes
-#
-#############################################################################################################
+"""
+Example BYND calculation: zinc phthalocyanine derivative (FHI-aims RT-TDDFT).
 
+Loads a long-time RT-TDDFT reference signal, extracts a short-time segment,
+runs the BYND frequency optimisation on that segment, adds a continuum
+correction, and writes a spectrum comparison plot (spectrum_bynd_vs_exact.pdf).
+
+All input files must be present in the working directory (copy from data/).
+"""
 import sys
 sys.path.append('../helpers/')
 sys.path.append('../src/')
-sys.path.append('../data/')
-import os
+
 import numpy as np
-import matplotlib.pyplot as plt
-from numpy.fft import fft, fftfreq
-from helpers_line_search import filter_frequencies, read_sma_data, read_RT_TDDFT_data
-from helpers_line_search import generate_signal_tensor, update_amplitudes_tensor, write_to_file
+from helpers_line_search import (
+    filter_frequencies, read_sma_data, read_RT_TDDFT_data,
+    generate_signal_tensor, update_amplitudes_tensor, write_to_file,
+    print_init_messages, print_iteration,
+)
 from simple_line_search import perform_line_search_tensor_off_diagonal
-from helpers_line_search import print_init_messages, print_iteration
 from continuum_amplitudes import get_continuum_freq
 from get_spectrum_time_domain import compute_spectrum_using_time_signal
 from plot_spectrum import plot_simple_spectrum
-# -----------------------------------------------------------------------------------------------------------
-# START
-# -----------------------------------------------------------------------------------------------------------
-# global settings
-# ------------------------------------------------------------------------------------------------------------
 
-final_time   = 4000. # length of the reference time signal in au
-n_time_steps = 20000 # total time steps
+# ----------------------------------------------------------------------------
+# Settings
+# ----------------------------------------------------------------------------
 
+final_time   = 4000.   # length of the reference time signal [au]
+n_time_steps = 20000   # total number of time steps
 
-# this is for cutting the short time signal out of the long time reference
-cut_start    = 100   # start point of the signal which we use as target in the optimization procedure
-cut_end      = 1500  # end point of the signal which we use as target in the optimization procedure
-#  It is helpful to cut the first piece of the signal, the electric field puls at the beginning
-#  sometimes causes unwanted spectral artifacts. This is also true for the end of the signal. Ideally,
-#  one would like to have the end of the signal to be zero. 
+# Short-time window used as the optimisation target.
+# Cutting the first steps avoids artefacts from the electric field pulse.
+cut_start = 100
+cut_end   = 1500
 
-# Read in all the data which must be in the current folder
-# ------------------------------------------------------------------------------------------------------------
-
-# Essential tasks which need to be done before optimization:
-# 1) We need to load the dipole data
-# 2) We need to load the SMA data
-# 3) We need to select the SMA narrow features 
-# 4) We need to prepare the short-time target signal for the optimization
-
-dipole_xyz, tddft_time = read_RT_TDDFT_data(t_unit_au=True) 
-sma_frq, sma_osc, sma_tmom = read_sma_data('sma_tddft.data',  'trans_mom_sma.data')
-
-
-time, dt = np.linspace(0., final_time, n_time_steps, retstep=True, endpoint=False)
-
-# we mutiply it with 100, gives nicer values for the amplitude output
-# we correct for it later on when we calculate the spectrum
-# just for example purposes
-dipole_xyz = 100* dipole_xyz[cut_start:cut_end,:]
-time = time[cut_start:cut_end]
-
-# Important, first we only select the sparse subset of the frequencies
-sma_frq, sma_tmom = filter_frequencies(sma_frq, sma_tmom, 1.5)
-
-amp_input = np.abs(sma_tmom[:,:]) # we x, y z
-freq_input = sma_frq
-
-# set target values of complete problem
-# the ordering is [xx, yy, zz, xy, xz, yz]
-target = np.zeros((len(time),6))
-target[:,0] = dipole_xyz[:,0]
-target[:,1] = dipole_xyz[:,4]
-target[:,2] = dipole_xyz[:,8]
-target[:,3] = dipole_xyz[:,1]
-target[:,4] = dipole_xyz[:,2]
-target[:,5] = dipole_xyz[:,5]
-
-# input amplitudes just for initialization
-amp_input, intercept_on = update_amplitudes_tensor(target[:,:3], freq_input, time, off_diagonal=False, intercept=True,method='ridge', reg_coef=20.01, ratio=0.8)
-amp_input_off, intercept_off = update_amplitudes_tensor(target[:,3:], freq_input, time, off_diagonal=True, intercept=True,method='ridge',reg_coef=20.01, ratio=0.8) 
-
-# perform optimization
-# ------------------------------------------------------------------------------------------------------------
-np.set_printoptions(precision=3)
-
-freq_sma_start = freq_input
-
-# set parameters
-iterations         = 85 # was 35
-switch_2_off       = 50
-search_radius_init = 0.05
-search_radius      = 0.001
+# Line-search parameters
+iterations         = 85
+switch_2_off       = 50       # iteration at which off-diagonal terms are included
+search_radius_init = 0.05     # search half-width for iteration 0 [au]
+search_radius      = 0.001    # search half-width for all later iterations [au]
 grid_spacing       = 0.00001
 
-# print out initial messages
-print_init_messages(iterations, switch_2_off, search_radius, search_radius_init, grid_spacing, freq_sma_start, len(target))
+# ----------------------------------------------------------------------------
+# Load input data
+# ----------------------------------------------------------------------------
 
-# start iterations
-# ------------------------------------------------------------------------------------------------------------
+dipole_xyz, _ = read_RT_TDDFT_data(t_unit_au=True)
+sma_frq, _, sma_tmom = read_sma_data('sma_tddft.data', 'trans_mom_sma.data')
 
-# Non linear-optimization is happening here:
+# Cut to the short-time optimisation window.
+# Dipole scaled by 100 for numerical convenience; corrected when computing
+# the final spectrum.
+time, _ = np.linspace(0., final_time, n_time_steps, retstep=True, endpoint=False)
+time       = time[cut_start:cut_end]
+dipole_xyz = 100 * dipole_xyz[cut_start:cut_end, :]
 
-#    - The first iteration has a search radius of 0.05
-#    - In the first iteration we also randomly modify the amplitudes
-#    - all other iterations have a search radius of 0.001
-#    - the search grid density is 0.00001
+sma_frq, sma_tmom = filter_frequencies(sma_frq, sma_tmom, threshold=1.5)
 
-#    This example is not checking if the result is converged
-#    We simply set the number of iterations to a sufficient high
-#    value.
+# ----------------------------------------------------------------------------
+# Assemble optimisation target  [xx, yy, zz, xy, xz, yz]
+# ----------------------------------------------------------------------------
 
-#    We also included the possibility to use "off-diagonal"
-#    signals at later iterations. This can improve the 
-#    convergence behaviour.
+target = np.zeros((len(time), 6))
+target[:, 0] = dipole_xyz[:, 0]   # xx
+target[:, 1] = dipole_xyz[:, 4]   # yy
+target[:, 2] = dipole_xyz[:, 8]   # zz
+target[:, 3] = dipole_xyz[:, 1]   # xy
+target[:, 4] = dipole_xyz[:, 2]   # xz
+target[:, 5] = dipole_xyz[:, 5]   # yz
 
-#    We include off-diagonal signals at iteration 50.
-#    This as a very safe choice. 
+freq_input     = sma_frq
+freq_sma_start = sma_frq.copy()   # fixed reference for the regularisation anchor
 
+amp_input, intercept_on = update_amplitudes_tensor(
+    target[:, :3], freq_input, time,
+    off_diagonal=False, intercept=True, method='ridge', reg_coef=20.01,
+)
+amp_input_off, intercept_off = update_amplitudes_tensor(
+    target[:, 3:], freq_input, time,
+    off_diagonal=True, intercept=True, method='ridge', reg_coef=20.01,
+)
+
+# ----------------------------------------------------------------------------
+# Line search optimisation
+# ----------------------------------------------------------------------------
+
+np.set_printoptions(precision=3)
+print_init_messages(
+    iterations, switch_2_off, search_radius, search_radius_init,
+    grid_spacing, freq_sma_start, len(target),
+)
 
 for i in range(iterations):
 
     if i == 0:
-
-        freq_input, amp_input, amp_input_off = perform_line_search_tensor_off_diagonal(target, amp_input, amp_input_off, freq_input, time, freq_sma_start, rf=search_radius_init, df=grid_spacing, diagonal_only=True, calc_intercept=True, reg_method='ridge', alpha_value=20.01, l1_ratio_value=0.8, initial='random')
-
-    elif i >= switch_2_off: # we also include the off diagonal elements
-
-        freq_input, amp_input, amp_input_off = perform_line_search_tensor_off_diagonal(target, amp_input, amp_input_off, freq_input, time, freq_sma_start, rf=search_radius, df=grid_spacing, diagonal_only=False, calc_intercept=True, reg_method='ridge', l1_ratio_value=20.8, alpha_value=20.01)
-
-    else: # only diagonal is considered
-
-        freq_input, amp_input, amp_input_off = perform_line_search_tensor_off_diagonal(target, amp_input, amp_input_off, freq_input, time, freq_sma_start, rf=search_radius, df=grid_spacing, diagonal_only=True, calc_intercept=True, reg_method='ridge', l1_ratio_value=20.8, alpha_value=20.01)
+        freq_input, amp_input, amp_input_off = perform_line_search_tensor_off_diagonal(
+            target, amp_input, amp_input_off, freq_input, time, freq_sma_start,
+            rf=search_radius_init, df=grid_spacing,
+            diagonal_only=True, calc_intercept=True,
+            reg_method='ridge', alpha_value=20.01, l1_ratio_value=0.8,
+            initial='random',
+        )
+    elif i >= switch_2_off:
+        freq_input, amp_input, amp_input_off = perform_line_search_tensor_off_diagonal(
+            target, amp_input, amp_input_off, freq_input, time, freq_sma_start,
+            rf=search_radius, df=grid_spacing,
+            diagonal_only=False, calc_intercept=True,
+            reg_method='ridge', alpha_value=20.01,
+        )
+    else:
+        freq_input, amp_input, amp_input_off = perform_line_search_tensor_off_diagonal(
+            target, amp_input, amp_input_off, freq_input, time, freq_sma_start,
+            rf=search_radius, df=grid_spacing,
+            diagonal_only=True, calc_intercept=True,
+            reg_method='ridge', alpha_value=20.01,
+        )
 
     print_iteration(i, freq_input, amp_input)
 
-
-# save narrow features results
-# ------------------------------------------------------------------------------------------------------------
-
-time, dt = np.linspace(0., final_time, n_time_steps, retstep=True, endpoint=False)
-signal = generate_signal_tensor(np.transpose(amp_input), freq_input, time, dipole=False, intercept=intercept_on)
-
-write_to_file('line_search_amp.out', amp_input, freq_input, intercept=intercept_on)
+write_to_file('line_search_amp.out',     amp_input,     freq_input, intercept=intercept_on)
 write_to_file('line_search_amp_off.out', amp_input_off, freq_input, off_diag=True, intercept=intercept_off)
 
+# ----------------------------------------------------------------------------
+# Continuum correction
+# ----------------------------------------------------------------------------
 
-# get the continuum frequencies
-# ------------------------------------------------------------------------------------------------------------
+signal     = generate_signal_tensor(amp_input.T,     freq_input, time, intercept=intercept_on)
+signal_off = generate_signal_tensor(amp_input_off.T, freq_input, time, intercept=intercept_off)
 
-# generate the signal which is going to be subtracted from the target signal
-time, dt = np.linspace(0., final_time, n_time_steps, retstep=True, endpoint=False)
-time = time[cut_start:cut_end]
-signal = generate_signal_tensor(np.transpose(amp_input), freq_input, time, dipole=False, intercept=intercept_on)
-signal_off = generate_signal_tensor(np.transpose(amp_input_off), freq_input, time, dipole=False, off_diagonal=True, intercept=intercept_off)
+# Reload with a looser threshold to cover the full SMA frequency grid
+sma_frq_all, _, sma_tmom_all = read_sma_data('sma_tddft.data', 'trans_mom_sma.data')
+sma_frq_all, sma_tmom_all = filter_frequencies(sma_frq_all, sma_tmom_all, threshold=0.07)
 
+amp_cont, intercept_cont, amp_cont_off, intercept_cont_off = get_continuum_freq(
+    target, signal, signal_off, time, sma_frq_all,
+)
 
-# get all sma frequencies
-sma_frq_all, sma_osc_all, sma_tmom_all = read_sma_data('sma_tddft.data',  'trans_mom_sma.data')
-# we do not need all of them we apply a minimum threshhold this filters out all
-# frequencies which do not have an amplitude anyway and saves memory and computing time
-sma_frq_all, sma_tmom_all = filter_frequencies(sma_frq_all, sma_tmom_all, 0.07)
-# get the amplitudes for the continuum region
-amp_input_continuum, intercept_on_continuum, amp_input_off_continuum, intercept_off_continuum = get_continuum_freq(target, signal, signal_off, time, sma_frq_all) 
+signal     += generate_signal_tensor(amp_cont.T,     sma_frq_all, time, intercept=intercept_cont)
+signal_off += generate_signal_tensor(amp_cont_off.T, sma_frq_all, time, intercept=intercept_cont_off)
 
-# calculate complete signal
-signal = signal + generate_signal_tensor(np.transpose(amp_input_continuum), sma_frq_all, time, dipole=False, intercept=intercept_on_continuum)
-signal_off = signal_off + generate_signal_tensor(np.transpose(amp_input_off_continuum), sma_frq_all, time, dipole=False, off_diagonal=True, intercept=intercept_off_continuum)
+write_to_file('line_search_amp_continuum.out',     amp_cont,     sma_frq_all, intercept=intercept_cont)
+write_to_file('line_search_amp_continuum_off.out', amp_cont_off, sma_frq_all, off_diag=True, intercept=intercept_cont_off)
 
-# write continuum information to file
-write_to_file('line_search_amp_continuum.out', amp_input_continuum, sma_frq_all, intercept=intercept_on_continuum)
-write_to_file('line_search_amp_continuum_off.out', amp_input_off_continuum, sma_frq_all, off_diag=True, intercept=intercept_off_continuum)
+# ----------------------------------------------------------------------------
+# Spectrum
+# ----------------------------------------------------------------------------
 
-# calculate, plot and save the spectrum
-# -----------------------------------------------------------------------------------------------------------
 compute_spectrum_using_time_signal(flag_plot_results=False)
 plot_simple_spectrum()
-# -----------------------------------------------------------------------------------------------------------
-# END
-# -----------------------------------------------------------------------------------------------------------
